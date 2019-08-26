0f0f56959674a7050e09836912c059449a15193e
natashakhatri <natasha.khatri@servian.com>
2019-08-26 11:28:28 +1000

    Added list of recipes using ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe('Spaghetti with Meat Sauce', 'Our favorite weeknight friendly spaghetti recipe', 'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit() {
