b7260f231618274f32339de46f2ea57e1d7b6536
natashakhatri <natasha.khatri@servian.com>
2019-08-30 13:29:41 +1000

    Added data persistence with firebase database.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,28 +9,34 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Spaghetti with Meat Sauce', 
-      'Our favorite weeknight friendly spaghetti recipe', 
-      'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg',
-      [
-        new Ingredient('Spaghetti', 1),
-        new Ingredient('Meat', 1),
-        new Ingredient('Tomatoes', 5)
-      ]),
-    new Recipe(
-      'Spaghetti Carbonara', 
-      'Another classic', 
-      'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg',
-      [
-        new Ingredient('Spaghetti', 1),
-        new Ingredient('Pancetta', 1),
-        new Ingredient('Eggs', 3)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Spaghetti with Meat Sauce', 
+  //     'Our favorite weeknight friendly spaghetti recipe', 
+  //     'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg',
+  //     [
+  //       new Ingredient('Spaghetti', 1),
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Tomatoes', 5)
+  //     ]),
+  //   new Recipe(
+  //     'Spaghetti Carbonara', 
+  //     'Another classic', 
+  //     'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg',
+  //     [
+  //       new Ingredient('Spaghetti', 1),
+  //       new Ingredient('Pancetta', 1),
+  //       new Ingredient('Eggs', 3)
+  //     ])
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
